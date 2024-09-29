df8b4b5a45fe0fd64fcbd162ba28ce09428d9889
krishna-nishant <nishantkrishna2005@gmail.com>
2024-09-29 21:26:08 +0530

    file upload

---
+++ 16_upload.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+    <form action="receive.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="myFile" id="">
+        <input type="submit" name="upload" value="SEND">
+    </form>
+</body>
+
+</html>
